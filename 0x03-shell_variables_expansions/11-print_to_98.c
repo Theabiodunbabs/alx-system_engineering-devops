5aa6e92cbc98d4933caab914705c7444f766949c
Theabiodunbabs <abiodunbabs8@gmail.com>
2023-07-19 04:59:34 -0700

    11 task

---
+++ 0x03-shell_variables_expansions/11-print_to_98.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * print_to_98 - Prints all natural numbers from n to 98
+ * @n: The starting number
+ */
+void print_to_98(int n)
+{
+	if (n <= 98)
+	{
+		for(int i = n; i < 98; i++)
+		{
+			printf("%d, ", i);
+		}
+	}
+	else
+	{
+		for(int i = n; i > 98; i--)
+		{
+			print("%d, ", i);
+		}
+	}
+	printf("98\n");
+}
